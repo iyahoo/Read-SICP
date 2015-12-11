ccc772d2dcfd5177d259b5fdf902afd8decb799e
iyahoo <s1200191@gmail.com>
2015-12-11 16:10:44 +0900

    [Add] 演習 1.2 ~ 1.5

---
+++ 1.scm
@@ -0,0 +1,26 @@
+(define size 2)
+
+(define my-list (lambda a a))
+
+(define (uuu x) (* x x))
+
+(define (square x) (* x x))
+
+(define (sum-of-squares x y)
+  (+ (square x) (square x)))
+
+;; 演習 1.2
+(define 1-2 (/ (+ 4 5 (- 2 (- 3 (+ 6 (/ 4 5))))) (* (- 2 7) (- 6 2) 3)))
+
+;; 演習 1.3 
+(define (big2 a b c)
+  (cond ((<= a b c) (+ (square b) (square c)))
+        ((<= b a c)) (+ (square a) (square c))
+        (else (+ (square a) (square b)))))
+
+;; 演習 1.4
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+;; 演習 1.5
+(define (p) (p))
