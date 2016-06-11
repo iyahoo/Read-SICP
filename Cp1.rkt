f6d941105355f4ef0db623048e90a1ddca4c19ba
iyahoo <s1200191@gmail.com>
2016-06-11 20:13:38 +0900

    [Add] 1.2.2

--- Cp1.rkt
+++ Cp1.rkt
@@ -68,3 +68,44 @@
 (define (1-8-cube-root x)  
   (1-8-sqrt-iter 1.0 x))
 
+;; 1.2.2
+
+;; 金種 = 5
+
+(define (count-change amount)
+  (cc amount 5))
+
+(define (cc amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc amount
+                     (- kinds-of-coins 1))
+                 (cc (- amount
+                        (first-denomination kinds-of-coins))
+                     kinds-of-coins)))))
+
+(define (first-denomination kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)
+        ((= kinds-of-coins 3) 10)
+        ((= kinds-of-coins 4) 25)
+        ((= kinds-of-coins 5) 50)))
+
+;; 金種 = 2
+
+(define (count-change2 amount)
+  (cc2 amount 2))
+
+(define (cc2 amount kinds-of-coins)
+  (cond ((= amount 0) 1)
+        ((or (< amount 0) (= kinds-of-coins 0)) 0)
+        (else (+ (cc2 amount
+                      (- kinds-of-coins 1))
+                 (cc2 (- amount
+                         (first-denomination2 kinds-of-coins))
+                      kinds-of-coins)))))
+
+(define (first-denomination2 kinds-of-coins)
+  (cond ((= kinds-of-coins 1) 1)
+        ((= kinds-of-coins 2) 5)))
+
