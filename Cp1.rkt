e4e5eb7858b788ca9787aa2dba5e6ca282d96871
iyahoo <s1200191@gmail.com>
2016-06-11 23:02:41 +0900

    [Add] 1.2.4

--- Cp1.rkt
+++ Cp1.rkt
@@ -109,3 +109,10 @@
   (cond ((= kinds-of-coins 1) 1)
         ((= kinds-of-coins 2) 5)))
 
+;; 1.2.4
+
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b ( fast-expt b (- n 1))))))
+
