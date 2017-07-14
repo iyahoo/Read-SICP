43fee16b5967ebd94b8dac1297f8740b916aef5f
iyahoo <s1200191@gmail.com>
2017-07-14 19:52:00 +0900

    [add] 2.1.1 練習問題

---
+++ Cp2/2_1_1.scm
@@ -0,0 +1,44 @@
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (cons (/ n g) (/ d g))))
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+;; 2.1
+
+(define (make-rat n d)
+  (let* ((abs-n (abs n))
+         (abs-d (abs d))
+         (g (gcd abs-n abs-d)))    
+    (let ((sign (if (> (* n d) 0) 1 -1)))
+      (cons (* sign (/ abs-n g)) (/ abs-d g)))))
+
