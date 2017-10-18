4eb8b91221d36d0e2a5d962b2f0adddb8e64be0b
iyahoo <s1200191@gmail.com>
2017-10-18 15:54:37 +0900

    [Done] 練習問題 2.36 まで

--- Cp2/2_2.scm
+++ Cp2/2_2.scm
@@ -550,3 +550,40 @@
 (test* "my-length2" 0 (my-length nil))
 (test* "my-length3" 5 (my-length '(3 6 9 12 15)))
 
+;; ex 2.34
+
+(test-section "ex 2.34")
+
+(define (horner-eval x coefficient-sequence)
+  (accumulate (lambda (this-coeff higher-terms)
+                (+ this-coeff (* higher-terms x)))
+              0
+              coefficient-sequence))
+
+(test* "horner-eval" 79 (horner-eval 2 (list 1 3 0 5 0 1)))
+
+;; ex 2.35
+
+(test-section "ex 2.35")
+
+(define (count-leaves-acc t)
+  (accumulate + 0 (map (^[x] 1) (enumerate-tree t))))
+
+(test* "count-leaves1" 4 (count-leaves-acc (cons (list 1 2) (list 3 4))))
+(test* "count-leaves2" 8 (count-leaves-acc (list (cons (list 1 2) (list 3 4))
+                                                 (cons (list 1 2) (list 3 4)))))
+
+;; ex 2.36
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(test* "accumulate-n" '(22 26 30) (accumulate-n + 0 '((1 2 3) (4 5 6) (7 8 9) (10 11 12))))
+
+;; ex 2.37
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
